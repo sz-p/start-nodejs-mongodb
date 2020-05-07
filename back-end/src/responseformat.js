8ee4b7450009daba922c285e64ae5cba4f8f301e
sz-p <sz_p@outlook.com>
2020-05-07 16:11:02 +0800

    [refactor]: 格式化部分代码

--- back-end/src/responseformat.js
+++ back-end/src/responseformat.js
@@ -1,20 +1,21 @@
 export default {
-  success: function (msg) {
-    return {
-      success: true,
-      status: 0,
-      api: msg.api,
-      information: msg.infor,
-      data: msg.data
-    }
-  },
-  error: function (msg) {
-    return {
-      success: false,
-      status: -1,
-      api: msg.api,
-      information: msg.infor,
-      data: msg.data
-    }
-  }
-}
+	success: function(msg) {
+		return {
+			success: true,
+			status: 0,
+			api: msg.api,
+			information: msg.infor,
+			data: msg.data
+		};
+	},
+	error: function(msg) {
+		return {
+			success: false,
+			status: -1,
+			api: msg.api,
+			information: msg.infor,
+			data: msg.data,
+			errors: msg.errors
+		};
+	}
+};
