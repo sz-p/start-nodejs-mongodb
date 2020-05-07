6ed8554593213ec4d228a11cc629cc4acc391ddc
sz-p <sz_p@outlook.com>
2020-05-07 16:17:42 +0800

    [feat]: 数据库写入操作

--- back-end/src/routers/get-all-data/getdata-all-data.js
+++ back-end/src/routers/get-all-data/getdata-all-data.js
@@ -1,18 +1,18 @@
 import RP from '../../responseformat';
-import { testCollectionModel } from '../../actions/mongondb/collections';
+import { newModel } from '../../actions/mongondb/collections';
 
 /**
  * set to mongodb
  *
  */
-export default function (requestData) {
-  return new Promise((resolve, reject) => {
-    testCollectionModel.find({}, (err, data) => {
-      if (err) {
-        reject(RP.error({ api: 'getalldata', data }))
-      } else {
-        resolve(RP.success({ api: 'getalldata', data }));
-      }
-    })
-  });
+export default function(requestData) {
+	return new Promise((resolve, reject) => {
+		newModel.find({}, (err, data) => {
+			if (err) {
+				reject(RP.error({ api: 'getalldata', data }));
+			} else {
+				resolve(RP.success({ api: 'getalldata', data }));
+			}
+		});
+	});
 }
--- back-end/src/routers/set2db/set2db.js
+++ back-end/src/routers/set2db/set2db.js
@@ -1,19 +1,39 @@
 import RP from '../../responseformat';
-import { testCollectionModel } from '../../actions/mongondb/collections';
+import { newModel } from '../../actions/mongondb/collections';
 
 /**
  * set to mongodb
  *
  */
-export default function (requestData) {
-  return new Promise((resolve, reject) => {
-
-    testCollectionModel.save(requestData, (err, data) => {
-      if (err) {
-        reject(RP.error({ api: 'set2db', data }))
-      } else {
-        resolve(RP.success({ api: 'set2db', data }));
-      }
-    })
-  });
+export default function(requestData) {
+	return new Promise((resolve, reject) => {
+		let saveData = new newModel({
+			UUID: '1',
+			testFunction: function() {
+				console.log('fun');
+			},
+			testArrayFunction: () => {},
+			testExp: /i/g,
+			testdate: new Date(),
+			testArray: [
+				{
+					name: 'testArray first',
+					value: 1
+				},
+				{
+					name: 'testArray second',
+					value: 2
+				}
+			],
+			nan: NaN,
+			infinity: Infinity
+		});
+		saveData.save((err, data) => {
+			if (err) {
+				reject(RP.error({ api: 'set2db', data, errors: err.errors }));
+			} else {
+				resolve(RP.success({ api: 'set2db', data }));
+			}
+		});
+	});
 }
