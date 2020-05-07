c5db3d8bb3365f4e71ae75b4af90e8cc97f48808
sz-p <sz_p@outlook.com>
2020-05-07 16:14:59 +0800

    [feat]: 增加表结构

--- back-end/src/actions/mongondb/collections.js
+++ back-end/src/actions/mongondb/collections.js
@@ -1,26 +1,9 @@
 import mongoose from './connect';
 
 // console.log(mongoose.Schema.Types);
-const collectionDocument = new mongoose.Schema({
-  UUID: { type: String, index: true, required: true },
-  testFunction: { type: Object },
-  testExp: { type: Object },
-  testBASE64IMG: { type: String },
-  testdate: { type: Date, default: Date.now },
-  testArray: [{
-    name: { type: String, },
-    value: { type: Number }
-  }],
-  testJSON: {
-    type: JSON
-  },
-  testHTMLobj: {
-    type: Object
-  }
-})
+const newSchema = new mongoose.Schema({},{
+  versionKey: false,
+  strict: false
+});
 
-const testCollectionModel = mongoose.model('test', collectionDocument)
-
-export default {
-  testCollectionModel
-};
+export const newModel = mongoose.model('test', newSchema);
